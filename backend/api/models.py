806e8d26c664ceab1b3a61980e2f39fbe25cefbb
112306028 <147257600+112306028@users.noreply.github.com>
2026-08-03 19:26:51 +0800

    在申請加入時間欄位

--- backend/api/models.py
+++ backend/api/models.py
@@ -212,6 +212,7 @@ class Application(models.Model):
     campaign = models.ForeignKey(Campaigns, on_delete=models.CASCADE, db_column='campaign_id',related_name='applications')
     status = models.CharField(max_length=50, db_column='status', default='pending')
     reject_reason = models.TextField(blank=True, null=True, db_column='reject_reason')
+    created_at = models.DateTimeField(auto_now_add=True, db_column='created_at', null=True)
 
     class Meta:
         db_table = 'Application'
